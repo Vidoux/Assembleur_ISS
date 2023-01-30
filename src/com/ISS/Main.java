203e05529821b838434b8e4cc8ce3106d13c92eb
vidal <tanguy51.tv@gmail.com>
2023-01-30 16:04:21 +0100

    Changes by Tanguy

--- src/com/ISS/Main.java
+++ src/com/ISS/Main.java
@@ -1,8 +1,27 @@
 package com.ISS;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> strings = new ArrayList<String>();
+        try (Stream<String> stream = Files.lines(Paths.get("ressource/instructions.asm"))){
+
+            // convert stream into a string
+            String contents = stream.collect(Collectors.joining(System.lineSeparator()));
+
+            // print string
+            System.out.println(contents);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
